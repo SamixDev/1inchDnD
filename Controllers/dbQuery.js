6454bf6fefbb903b97032d888b25c6c6feb022e6
SamixDev <70034018+SamixDev@users.noreply.github.com>
2021-05-23 19:41:21 +0300

    added transactions api

--- Controllers/dbQuery.js
+++ Controllers/dbQuery.js
@@ -1,2 +1,24 @@
 const { connect } = require('../DataBase/connectSQL');
 
+async function transactions(intervalQuery, dbName, type) {
+    return new Promise((resolve, reject) => {
+        let sql = `SELECT ${intervalQuery} AS date, sum(val2) as value
+                    FROM ${dbName}
+                    where tx_type = '${type}'
+                    group by date;
+                    `
+
+        connect(sql).then(resp => {
+            if (resp == '') {
+                resolve({ data: [], msg: "No Data" })
+            } else {
+                resolve({ data: resp, msg: "Data Found" })
+            }
+        }).catch(err => {
+            console.log(err)
+            reject(err)
+        });
+    });
+};
+
+module.exports = { transactions }

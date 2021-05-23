72ab6eb131eb5830e7a297c41ecd6011529decb7
SamixDev <70034018+SamixDev@users.noreply.github.com>
2021-05-23 22:15:34 +0300

    prepare tnx count api

--- APIs/routes.js
+++ APIs/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const apiResponse = require("./apiResponse");
 const router = express.Router();
-const { transactions } = require('../Controllers/dbQuery')
+const { transactions, transactionsNumber } = require('../Controllers/dbQuery')
 
 router.get('/tnx', async (req, res) => {
 
@@ -47,4 +47,47 @@ router.get('/tnx', async (req, res) => {
     })
 });
 
+router.get('/tnxCount', async (req, res) => {
+
+    let chainId = req.query.chainId
+    let interval = req.query.interval
+    let type = req.query.type
+    let dbName;
+    let intervalQuery;
+    chainId === undefined ? 1 : chainId = Number(chainId.replace(/'|"/g, ""))
+    interval === undefined ? "monthly" : interval = interval.replace(/'|"/g, "")
+    type === undefined ? "Mint" : type = type.replace(/'|"/g, "")
+
+    switch (chainId) {
+        case 1: // eth
+            dbName = 'transactions_eth';
+            break;
+        case 56: // bsc
+            dbName = 'transactions_bsc';
+            break;
+        case 137: // polygon
+            dbName = 'transactions_pol';
+            break;
+        default:
+            dbName = 'transactions_eth';
+    }
+    switch (interval) {
+        case "Daily":
+            intervalQuery = 'LEFT(block_signed_at, 10)';
+            break;
+        case "Weekly":
+            intervalQuery = 'LEFT(block_signed_at, 9)'; // change later
+            break;
+        case "Monthly":
+            intervalQuery = 'LEFT(block_signed_at, 7)';
+            break;
+        default:
+            intervalQuery = 'LEFT(block_signed_at, 7)';
+    }
+    transactionsNumber(intervalQuery, dbName, type).then(({ data, msg }) => {
+        apiResponse.successResponseWithData(res, msg, data);
+    }).catch(err => {
+        apiResponse.ErrorResponse(err)
+    })
+});
 module.exports = router;
--- Controllers/dbQuery.js
+++ Controllers/dbQuery.js
@@ -21,4 +21,25 @@ async function transactions(intervalQuery, dbName, type) {
     });
 };
 
-module.exports = { transactions }
+async function transactionsNumber(intervalQuery, dbName, type) {
+    return new Promise((resolve, reject) => {
+        // let sql = `SELECT ${intervalQuery} AS date, sum(val2) as value
+        //             FROM ${dbName}
+        //             where tx_type = '${type}'
+        //             group by date;
+        //             `
+
+        // connect(sql).then(resp => {
+        //     if (resp == '') {
+        //         resolve({ data: [], msg: "No Data" })
+        //     } else {
+        //         resolve({ data: resp, msg: "Data Found" })
+        //     }
+        // }).catch(err => {
+        //     console.log(err)
+        //     reject(err)
+        // });
+    });
+};
+
+module.exports = { transactions, transactionsNumber }
